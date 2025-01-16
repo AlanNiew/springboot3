eaf72dc67072fce2b9d59c30fc9f3aea2263ec0d
Niu <728097735@qq.com>
2025-01-16 15:16:06 +0800

    impl

---
+++ src/main/java/com/example/springboot3/impl/MyServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.springboot3.impl;
+
+import com.example.springboot3.service.MyService;
+
+/**
+ * Author:Niu
+ * Date:2025/1/16 15:15
+ */
+public class MyServiceImpl implements MyService {
+}
