0280306c8ddba4d3a008575397f1c93c0485ec0a
Niu <728097735@qq.com>
2025-01-16 15:14:38 +0800

    My

---
+++ src/main/java/com/example/springboot3/service/MyService.java
@@ -0,0 +1,8 @@
+package com.example.springboot3.service;
+
+/**
+ * Author:Niu
+ * Date:2025/1/16 15:09
+ */
+public interface MyService {
+}
---
+++ src/main/java/com/example/springboot3/util/MyUtil.java
@@ -0,0 +1,14 @@
+package com.example.springboot3.util;
+
+import java.time.LocalDateTime;
+
+/**
+ * Author:Niu
+ * Date:2025/1/16 15:10
+ */
+public class MyUtil {
+
+    public static String getKey() {
+        return LocalDateTime.now().toString()+System.currentTimeMillis()/1000;
+    }
+}
